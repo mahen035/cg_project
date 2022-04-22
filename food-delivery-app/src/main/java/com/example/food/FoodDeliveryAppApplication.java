486cf8e917e0531e992b7f55b7578f9d6fa4741a
mkumar1 <abc1@mail.com>
2022-04-22 12:36:30 +0530

    from demo1

--- food-delivery-app/src/main/java/com/example/food/FoodDeliveryAppApplication.java
+++ food-delivery-app/src/main/java/com/example/food/FoodDeliveryAppApplication.java
@@ -3,6 +3,7 @@ package com.example.food;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//mkumar1:commenting for merge demo
 @SpringBootApplication
 public class FoodDeliveryAppApplication {
 
