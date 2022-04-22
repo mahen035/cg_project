0cd41decc39d79c16b79cd03f499f95816b4adf1
mkumar12 <abc@mail.com>
2022-04-22 14:18:34 +0530

    added sysout

--- food-delivery-app/src/main/java/com/example/food/FoodDeliveryAppApplication.java
+++ food-delivery-app/src/main/java/com/example/food/FoodDeliveryAppApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodDeliveryAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This code is to demonstrate git merge");
 		SpringApplication.run(FoodDeliveryAppApplication.class, args);
 	}
 
